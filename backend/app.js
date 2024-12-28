35f0355ae878edeba30830a99c20a290c7f83841
spreethi01 <121300794+spreethi01@users.noreply.github.com>
2024-12-28 16:25:51 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,26 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+const bookingRoutes = require('./routes/bookingRoutes');
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/bookings', bookingRoutes);
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log(err));
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
